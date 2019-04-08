319a6710b30ab917111e75283da53e5a4e925f9e
Rodrigo M <rodrigo.medina.neri@gmail.com>
2019-04-08 12:31:12 -0500

    Flat map issue

--- index.js
+++ index.js
@@ -2,4 +2,15 @@
 
 const { main } = require('./lib/main');
 // Archivo principal de ejecución :)
+
+const concat = (x,y) =>
+  x.concat(y)
+
+const flatMap = (f,xs) =>
+  xs.map(f).reduce(concat, [])
+
+Array.prototype.flatMap = function(f) {
+  return flatMap(f,this)
+}
+
 main();
